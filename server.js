628491db594e865fa71d613ba189a0f29d6c03c8
Elliot Park <elliotpark410@gmail.com>
2022-02-09 23:08:10 -0800

    fix bug in delete route for tags

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+// force: true - adds a DROP TABLE IF EXISTS before tying to create the table
+// force: false - if the table exists, any field additions/modifications/deletions you have won't be executed.
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}'`));
 });
